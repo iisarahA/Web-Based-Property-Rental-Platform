9d1164e9c28df876bed27ef1f517833af045b629
iisarahA <128263250+iisarahA@users.noreply.github.com>
2024-01-30 21:31:08 +0300

    Add files via upload

---
+++ Web2_Project_suhad 2 2/login.js
@@ -0,0 +1,19 @@
+
+function redirect(){
+var role = document.getElementById("role").value;
+if(role == "homeOwner")
+{window.open("homeO.html");} //add Home owner page here
+else
+{ window.open("homeS.html");} // add Home Seeker page here
+
+}//end of redirect function 
+
+function signup()
+{
+    window.open("login.html")
+}
+
+function AddProp()
+{
+    window.open("homeO.html")
+}
